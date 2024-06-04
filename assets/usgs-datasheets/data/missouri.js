33db7cd68a13a070925802a53acb19cb0e003c04
Jennifer Galas <jennifer.galas@scranton.edu>
2024-06-04 19:38:24 -0400

    Begin adding MO datasheets

---
+++ assets/usgs-datasheets/data/missouri.js
@@ -0,0 +1,51 @@
+var geojsonFeature = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-89.5,36],[-89.5,36.25],[-89.75,36.25],[-89.75,36],[-89.5,36]]]
+        },
+        "properties": {
+          "name": "Caruthersville",
+          "vertical": "https://thesurveystation.com/USGS/MO/1-Caruthersville/Vertical.pdf",
+          "vyear": "1969",
+          "horizontal": "https://thesurveystation.com/USGS/MO/1-Caruthersville/Horizontal.pdf",
+          "hyear": "1970",
+          "coe": "https://thesurveystation.com/USGS/MO/1-Caruthersville/Corps-of-Engineers.pdf",
+          "cyear": "1967",
+          "recoveries": "https://thesurveystation.com/USGS/MO/1-Caruthersville/Tri-Station-1028-Shade.pdf",
+          "ryear": "1972"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-89.75,36],[-89.75,36.25],[-90,36.25],[-90,36],[-89.75,36]]]
+        },
+        "properties": {
+          "name": "Hayti",
+          "vertical": "https://thesurveystation.com/USGS/MO/2-Hayti/Vertical.pdf",
+          "vyear": "1976",
+          "horizontal": "https://thesurveystation.com/USGS/MO/2-Hayti/Horizontal.pdf",
+          "hyear": "1970",
+          "coe": "https://thesurveystation.com/USGS/MO/2-Hayti/Corps-of-Engineers.pdf",
+          "cyear": "1971"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-90,36],[-90,36.25],[-90.25,36.25],[-90.25,36],[-90,36]]]
+        },
+        "properties": {
+          "name": "Kennett",
+          "vertical": "https://thesurveystation.com/USGS/MO/3-Kennett/Vertical.pdf",
+          "vyear": "1982"
+        }
+      }    
+    ]
+  };
