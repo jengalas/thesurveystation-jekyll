51436d9289f7b33f5edebc6936a407ddef0f50ec
Jennifer Galas <jennifer.galas@scranton.edu>
2024-06-05 21:16:21 -0400

    Add more MO datasheets

--- assets/usgs-datasheets/data/missouri.js
+++ assets/usgs-datasheets/data/missouri.js
@@ -46,6 +46,92 @@ var geojsonFeature = {
           "vertical": "https://thesurveystation.com/USGS/MO/3-Kennett/Vertical.pdf",
           "vyear": "1982"
         }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-90.25,36],[-90.25,36.25],[-90.5,36.25],[-90.5,36],[-90.25,36]]]
+        },
+        "properties": {
+          "name": "Marmaduke",
+          "vertical": "https://thesurveystation.com/USGS/MO/4-Marmaduke/Vertical.pdf",
+          "vyear": "1980"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-89.25,36.25],[-89.25,36.5],[-89.5,36.5],[-89.5,36.25],[-89.25,36.25]]]
+        },
+        "properties": {
+          "name": "Reelfoot Lake",
+          "vertical": "https://thesurveystation.com/USGS/MO/5-Reelfoot-Lake/Vertical.pdf",
+          "vyear": "1975",
+          "horizontal": "https://thesurveystation.com/USGS/MO/5-Reelfoot-Lake/Horizontal.pdf",
+          "hyear": "1970, 1977"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-89.5,36.25],[-89.5,36.5],[-89.75,36.5],[-89.75,36.25],[-89.5,36.25]]]
+        },
+        "properties": {
+          "name": "Portageville",
+          "vertical": "https://thesurveystation.com/USGS/MO/6-Portageville/Vertical.pdf",
+          "vyear": "1971",
+          "horizontal": "https://thesurveystation.com/USGS/MO/6-Portageville/Horizontal.pdf",
+          "hyear": "1970",
+          "coe": "https://thesurveystation.com/USGS/MO/6-Portageville/Corps-of-Engineers.pdf",
+          "cyear": "1954"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-89.75,36.25],[-89.75,36.5],[-90,36.5],[-90,36.25],[-89.75,36.25]]]
+        },
+        "properties": {
+          "name": "Pascola",
+          "vertical": "https://thesurveystation.com/USGS/MO/7-Pascola/Vertical.pdf",
+          "vyear": "1976",
+          "horizontal": "https://thesurveystation.com/USGS/MO/7-Pascola/Horizontal.pdf",
+          "hyear": "1970",
+          "coe": "https://thesurveystation.com/USGS/MO/7-Pascola/Corps-of-Engineers.pdf",
+          "cyear": "1954"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-90,36.25],[-90,36.5],[-90.25,36.5],[-90.25,36.25],[-90,36.25]]]
+        },
+        "properties": {
+          "name": "Piggot",
+          "vertical": "https://thesurveystation.com/USGS/MO/8-Piggot/Vertical.pdf",
+          "vyear": "1976"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-89,36.5],[-89,36.75],[-89.25,36.75],[-89.25,36.5],[-89,36.5]]]
+        },
+        "properties": {
+          "name": "Hickman",
+          "vertical": "https://thesurveystation.com/USGS/MO/9-Hickman/Vertical.pdf",
+          "vyear": "1974",
+          "horizontal": "https://thesurveystation.com/USGS/MO/9-Hickman/Horizontal.pdf",
+          "hyear": "1978",
+          "coe": "https://thesurveystation.com/USGS/MO/9-Hickman/Corps-of-Engineers.pdf",
+          "cyear": "1953"
+        }
       }    
     ]
   };
