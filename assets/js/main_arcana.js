3c4fb32b09225b8ede96a86cbab684cdc60212a7
Jennifer Galas <jennifer.galas@scranton.edu>
2024-10-20 20:06:18 -0400

    Start working with theme

---
+++ assets/js/main_arcana.js
@@ -0,0 +1,18 @@
+/*
+	Arcana by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	// Breakpoints.
+		breakpoints({
+			wide:      [ '1281px',  '1680px' ],
+			normal:    [ '981px',   '1280px' ],
+			narrow:    [ '841px',   '980px'  ],
+			narrower:  [ '737px',   '840px'  ],
+			mobile:    [ '481px',   '736px'  ],
+			mobilep:   [ null,      '480px'  ]
+		});
+})(jQuery);
