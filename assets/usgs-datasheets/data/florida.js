ac02044cc524475c081f4f18c69f4f3cc6df44d8
Jennifer Galas <jennifer.galas@scranton.edu>
2025-02-13 21:52:04 -0500

    Add FL datasheets

---
+++ assets/usgs-datasheets/data/florida.js
@@ -0,0 +1,41 @@
+var geojsonFeature = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.25,25],[-80.25,25.25],[-80.5,25.25],[-80.5,25],[-80.25,25]]]
+        },
+        "properties": {
+          "name": "Garden Cove",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/12-Garden-Cove/Vertical.pdf",
+          "vyear": "1973"
+        }
+      },   
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.75,25],[-80.75,25.25],[-81,25.25],[-81,25],[-80.75,25]]]
+        },
+        "properties": {
+          "name": "Flamingo",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/14-Flamingo/Vertical.pdf",
+          "vyear": "1972"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-81,25],[-81,25.25],[-81.25,25.25],[-81.25,25],[-81,25]]]
+        },
+        "properties": {
+          "name": "Cape Sable",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/15-Cape-Sable/Vertical.pdf",
+          "vyear": "1972"
+        }
+      }   
+    ]
+  };
