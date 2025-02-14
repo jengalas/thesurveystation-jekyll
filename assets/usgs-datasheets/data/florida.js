8d9df6d1dbd7694724bc1881260b8c015bb80689
Jennifer Galas <jennifer.galas@scranton.edu>
2025-02-14 13:49:01 -0500

    Add FL quads

--- assets/usgs-datasheets/data/florida.js
+++ assets/usgs-datasheets/data/florida.js
@@ -36,6 +36,78 @@ var geojsonFeature = {
           "vertical": "https://geodata.thesurveystation.com/USGS/FL/15-Cape-Sable/Vertical.pdf",
           "vyear": "1972"
         }
-      }   
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.25,25.25],[-80.25,25.5],[-80.5,25.5],[-80.5,25.25],[-80.25,25.25]]]
+        },
+        "properties": {
+          "name": "Homestead",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/17-Homestead/Vertical.pdf",
+          "vyear": "1977"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.5,25.25],[-80.5,25.5],[-80.75,25.5],[-80.75,25.25],[-80.5,25.25]]]
+        },
+        "properties": {
+          "name": "Royal Palm Ranger Station",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/18-Royal-Palm-Ranger-Station/Vertical.pdf",
+          "vyear": "1972"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.75,25.25],[-80.75,25.5],[-81,25.5],[-81,25.25],[-80.75,25.25]]]
+        },
+        "properties": {
+          "name": "WW",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/19-WW/Vertical.pdf",
+          "vyear": "1972"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.25,25.5],[-80.25,25.75],[-80.5,25.75],[-80.5,25.5],[-80.25,25.5]]]
+        },
+        "properties": {
+          "name": "South Miami",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/22-South-Miami/Vertical.pdf",
+          "vyear": "1973"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.25,25.75],[-80.25,26],[-80.5,26],[-80.5,25.75],[-80.25,25.75]]]
+        },
+        "properties": {
+          "name": "Hialeah",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/28-Hialeah/Vertical.pdf",
+          "vyear": "1973"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.75,25.75],[-80.75,26],[-81,26],[-81,25.75],[-80.75,25.75]]]
+        },
+        "properties": {
+          "name": "RR",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/30-RR/Vertical.pdf",
+          "vyear": "1971"
+        }
+      }
     ]
   };
