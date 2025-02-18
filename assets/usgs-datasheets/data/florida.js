48206fbcf637d30be4ad85170e0ef313389314e5
Jennifer Galas <jennifer.galas@scranton.edu>
2025-02-17 21:45:32 -0500

    Add FL quads

--- assets/usgs-datasheets/data/florida.js
+++ assets/usgs-datasheets/data/florida.js
@@ -372,6 +372,306 @@ var geojsonFeature = {
           "vertical": "https://geodata.thesurveystation.com/USGS/FL/62-Neptune/Vertical.pdf",
           "vyear": "1971"
         }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.25,26.75],[-80.25,27],[-80.5,27],[-80.5,26.75],[-80.25,26.75]]]
+        },
+        "properties": {
+          "name": "Z",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/63-Z/Vertical.pdf",
+          "vyear": "1971"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.5,26.75],[-80.5,27],[-80.75,27],[-80.75,26.75],[-80.5,26.75]]]
+        },
+        "properties": {
+          "name": "Torry Island",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/64-Torry-Island/Vertical.pdf",
+          "vyear": "1971"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-81,26.75],[-81,27],[-81.25,27],[-81.25,26.75],[-81,26.75]]]
+        },
+        "properties": {
+          "name": "Lake Hicpochee",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/66-Lake-Hicpochee/Vertical.pdf",
+          "vyear": "1971"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-81.25,26.75],[-81.25,27],[-81.5,27],[-81.5,26.75],[-81.25,26.75]]]
+        },
+        "properties": {
+          "name": "La Belle",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/67-La-Belle/Vertical.pdf",
+          "vyear": "1971"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-81.5,26.75],[-81.5,27],[-81.75,27],[-81.75,26.75],[-81.5,26.75]]]
+        },
+        "properties": {
+          "name": "Telegraph Swamp",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/68-Telegraph-Swamp/Vertical.pdf",
+          "vyear": "1973"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-81.75,26.75],[-81.75,27],[-82,27],[-82,26.75],[-81.5,26.75]]]
+        },
+        "properties": {
+          "name": "Glen",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/69-Glen/Vertical.pdf",
+          "vyear": "1976"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-82,26.75],[-82,27],[-82.25,27],[-82.25,26.75],[-82,26.75]]]
+        },
+        "properties": {
+          "name": "Punta Gorda",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/70-Punta-Gorda/Vertical.pdf",
+          "vyear": "1978"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-82.25,26.75],[-82.25,27],[-82.5,27],[-82.5,26.75],[-82.25,26.75]]]
+        },
+        "properties": {
+          "name": "Englewood",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/71-Englewood/Vertical.pdf",
+          "vyear": "1978"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80,27],[-80,27.25],[-80.25,27.25],[-80.25,27],[-80,27]]]
+        },
+        "properties": {
+          "name": "Gomez",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/72-Gomez/Vertical.pdf",
+          "vyear": "1973"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.25,27],[-80.25,27.25],[-80.5,27.25],[-80.5,27],[-80.25,27]]]
+        },
+        "properties": {
+          "name": "Indian Town",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/73-Indian-Town/Vertical.pdf",
+          "vyear": "1977"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.5,27],[-80.5,27.25],[-80.75,27.25],[-80.75,27],[-80.5,27]]]
+        },
+        "properties": {
+          "name": "Okeechobee No. 4",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/74-Okeechobee-No-4/Vertical.pdf",
+          "vyear": "1973"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.75,27],[-80.75,27.25],[-81,27.25],[-81,27],[-80.75,27]]]
+        },
+        "properties": {
+          "name": "Okeechobee",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/75-Okeechobee/Vertical.pdf",
+          "vyear": "1973"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-81.25,27],[-81.25,27.25],[-81.5,27.25],[-81.5,27],[-81.25,27]]]
+        },
+        "properties": {
+          "name": "Venus",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/77-Venus/Vertical.pdf",
+          "vyear": "1973"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-81.5,27],[-81.5,27.25],[-81.75,27.25],[-81.75,27],[-81.5,27]]]
+        },
+        "properties": {
+          "name": "Long Island Marsh",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/78-Long-Island-Marsh/Vertical.pdf",
+          "vyear": "1973"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-81.75,27],[-81.75,27.25],[-82,27.25],[-82,27],[-81.75,27]]]
+        },
+        "properties": {
+          "name": "Arcadia",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/79-Arcadia/Vertical.pdf",
+          "vyear": "1973"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-82,27],[-82,27.25],[-82.25,27.25],[-82.25,27],[-82,27]]]
+        },
+        "properties": {
+          "name": "Murdock",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/80-Murdock/Vertical.pdf",
+          "vyear": "1972"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-82.25,27],[-82.25,27.25],[-82.5,27.25],[-82.5,27],[-82.25,27]]]
+        },
+        "properties": {
+          "name": "Venice",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/81-Venice/Vertical.pdf",
+          "vyear": "1972"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.25,27.25],[-80.25,27.5],[-80.5,27.5],[-80.5,27.25],[-80.25,27.25]]]
+        },
+        "properties": {
+          "name": "Fort Pierce",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/84-Fort-Pierce/Vertical.pdf",
+          "vyear": "1973"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.5,27.25],[-80.5,27.5],[-80.75,27.5],[-80.75,27.25],[-80.5,27.25]]]
+        },
+        "properties": {
+          "name": "Okeechobee No. 1",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/85-Okeechobee-No-1/Vertical.pdf",
+          "vyear": "1973"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-80.75,27.25],[-80.75,27.5],[-81,27.5],[-81,27.25],[-80.75,27.25]]]
+        },
+        "properties": {
+          "name": "Taylor Creek",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/86-Taylor-Creek/Vertical.pdf",
+          "vyear": "1973"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-81.25,27.25],[-81.25,27.5],[-81.5,27.5],[-81.5,27.25],[-81.25,27.25]]]
+        },
+        "properties": {
+          "name": "Lake Istokpoga",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/88-Lake-Istokpoga/Vertical.pdf",
+          "vyear": "1978"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-81.5,27.25],[-81.5,27.5],[-81.75,27.5],[-81.75,27.25],[-81.5,27.25]]]
+        },
+        "properties": {
+          "name": "Crewsville",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/89-Crewsville/Vertical.pdf",
+          "vyear": "1978"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-81.75,27.25],[-81.75,27.5],[-82,27.5],[-82,27.25],[-81.75,27.25]]]
+        },
+        "properties": {
+          "name": "Gardner",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/90-Gardner/Vertical.pdf",
+          "vyear": "1972"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-82,27.25],[-82,27.5],[-82.25,27.5],[-82.25,27.25],[-82,27.25]]]
+        },
+        "properties": {
+          "name": "Myakka City",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/91-Myakka-City/Vertical.pdf",
+          "vyear": "1978"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-82.25,27.25],[-82.25,27.5],[-82.5,27.5],[-82.5,27.25],[-82.25,27.25]]]
+        },
+        "properties": {
+          "name": "Fruitville",
+          "vertical": "https://geodata.thesurveystation.com/USGS/FL/92-Fruitville/Vertical.pdf",
+          "vyear": "1972"
+        }
       }
     ]
   };
