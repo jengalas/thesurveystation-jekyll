c85e23e41ffe946dbe622daa3d210130d9117887
Jennifer Galas <jennifer.galas@scranton.edu>
2025-03-21 09:19:21 -0400

    Begin adding NE quads

---
+++ assets/usgs-datasheets/data/nebraska.js
@@ -0,0 +1,145 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-95.25,40],[-95.25,40.25],[-95.5,40.25],[-95.5,40],[-95.25,40]]]
+      },
+      "properties": {
+        "name": "Craig",
+        "vertical": "https://geodata.thesurveystation.com/USGS/NE/1-Craig/Vertical.pdf",
+        "vyear": "1975",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/NE/1-Craig/Horizontal.pdf",
+        "hyear": "1964"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-95.5,40],[-95.5,40.25],[-95.75,40.25],[-95.75,40],[-95.5,40]]]
+      },
+      "properties": {
+        "name": "Falls City",
+        "vertical": "https://geodata.thesurveystation.com/USGS/NE/2-Falls-City/Vertical.pdf",
+        "vyear": "1965",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/NE/2-Falls-City/Horizontal.pdf",
+        "hyear": "1964"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-95.75,40],[-95.75,40.25],[-96,40.25],[-96,40],[-95.75,40]]]
+      },
+      "properties": {
+        "name": "Humboldt",
+        "vertical": "https://geodata.thesurveystation.com/USGS/NE/3-Humboldt/Vertical.pdf",
+        "vyear": "1964",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/NE/3-Humboldt/Horizontal.pdf",
+        "hyear": "1964"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-96,40],[-96,40.25],[-96.25,40.25],[-96.25,40],[-96,40]]]
+      },
+      "properties": {
+        "name": "Tecumseh No. 4",
+        "vertical": "https://geodata.thesurveystation.com/USGS/NE/4-Tecumseh-No-4/Vertical.pdf",
+        "vyear": "1964",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/NE/4-Tecumseh-No-4/Horizontal.pdf",
+        "hyear": "1964"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-96.25,40],[-96.25,40.25],[-96.5,40.25],[-96.5,40],[-96.25,40]]]
+      },
+      "properties": {
+        "name": "Liberty",
+        "vertical": "https://geodata.thesurveystation.com/USGS/NE/5-Liberty/Vertical.pdf",
+        "vyear": "1964",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/NE/5-Liberty/Horizontal.pdf",
+        "hyear": "1964"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-96.5,40],[-96.5,40.25],[-96.75,40.25],[-96.75,40],[-96.5,40]]]
+      },
+      "properties": {
+        "name": "Wymore",
+        "vertical": "https://geodata.thesurveystation.com/USGS/NE/6-Wymore/Vertical.pdf",
+        "vyear": "1969",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/NE/6-Wymore/Horizontal.pdf",
+        "hyear": "1965"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-96.75,40],[-96.75,40.25],[-97,40.25],[-97,40],[-96.75,40]]]
+      },
+      "properties": {
+        "name": "Odell",
+        "vertical": "https://geodata.thesurveystation.com/USGS/NE/7-Odell/Vertical.pdf",
+        "vyear": "1971",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/NE/7-Odell/Horizontal.pdf",
+        "hyear": "1968"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-97,40],[-97,40.25],[-97.25,40.25],[-97.25,40],[-97,40]]]
+      },
+      "properties": {
+        "name": "Fairbury",
+        "vertical": "https://geodata.thesurveystation.com/USGS/NE/8-Fairbury/Vertical.pdf",
+        "vyear": "1958",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/NE/8-Fairbury/Horizontal.pdf",
+        "hyear": "1968"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-97.25,40],[-97.25,40.25],[-97.5,40.25],[-97.5,40],[-97.25,40]]]
+      },
+      "properties": {
+        "name": "Fairbury No. 3",
+        "vertical": "https://geodata.thesurveystation.com/USGS/NE/9-Fairbury-No-3/Vertical.pdf",
+        "vyear": "1964",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/NE/9-Fairbury-No-3/Horizontal.pdf",
+        "hyear": "1965"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-97.5,40],[-97.5,40.25],[-97.75,40.25],[-97.75,40],[-97.5,40]]]
+      },
+      "properties": {
+        "name": "Hebron",
+        "vertical": "https://geodata.thesurveystation.com/USGS/NE/10-Hebron/Vertical.pdf",
+        "vyear": "1964",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/NE/10-Hebron/Horizontal.pdf",
+        "hyear": "1964"
+      }
+    }                   
+  ]
+};
