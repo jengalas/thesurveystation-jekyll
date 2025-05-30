edf14d5accb4bd1a176135bc6d57dfbe00cbda98
Jennifer Galas <jennifer.galas@scranton.edu>
2025-05-29 20:23:46 -0400

    Begin SD quads

---
+++ assets/usgs-datasheets/data/south-dakota.js
@@ -0,0 +1,47 @@
+var geojsonFeature = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-96.25,42.25],[-96.25,42.5],[-96.5,42.5],[-96.5,42.25],[-96.25,42.25]]]
+        },
+        "properties": {
+          "name": "Sioux City South",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/1-Sioux-City-South/Vertical.pdf",
+          "vyear": "1966",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/1-Sioux-City-South/Horizontal.pdf",
+          "hyear": "1962"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-96.5,42.25],[-96.5,42.5],[-96.75,42.5],[-96.75,42.25],[-96.5,42.25]]]
+        },
+        "properties": {
+          "name": "Emerson",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/2-Emerson/Vertical.pdf",
+          "vyear": "1966",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/2-Emerson/Horizontal.pdf",
+          "hyear": "1967"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-96.75,42.25],[-96.75,42.5],[-97,42.5],[-97,42.25],[-96.75,42.25]]]
+        },
+        "properties": {
+          "name": "Sioux City North",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/3-Sioux-City-North/Vertical.pdf",
+          "vyear": "1962",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/3-Sioux-City-North/Horizontal.pdf",
+          "hyear": "1967"
+        }
+      }    
+    ]
+  };
