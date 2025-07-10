9e0fc26ee06e1fdb1f47c29986777b9ed04be3ff
Jennifer Galas <jennifer.galas@scranton.edu>
2025-07-10 17:50:22 -0400

    Begin adding Wyoming quads

--- assets/usgs-datasheets/data/wyoming.js
+++ assets/usgs-datasheets/data/wyoming.js
@@ -1,103 +1,173 @@
 var geojsonFeature = {
-    "type": "FeatureCollection",
-    "features": [
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Polygon",
-          "coordinates": [[[-104,43.25],[-104,43.5],[-104.25,43.5],[-104.25,43.25],[-104,43.25]]]
-        },
-        "properties": {
-          "name": "Mule Creek",
-          "vertical": "https://geodata.thesurveystation.com/USGS/WY/262-Mule-Creek/Vertical.pdf",
-          "vyear": "1970",
-          "horizontal": "https://geodata.thesurveystation.com/USGS/WY/262-Mule-Creek/Horizontal.pdf",
-          "hyear": "1957"
-        }
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-104, 43.25], [-104, 43.5], [-104.25, 43.5], [-104.25, 43.25], [-104, 43.25]]]
       },
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Polygon",
-          "coordinates": [[[-104,43.5],[-104,43.75],[-104.25,43.75],[-104.25,43.5],[-104,43.5]]]
-        },
-        "properties": {
-          "name": "Dewey",
-          "vertical": "https://geodata.thesurveystation.com/USGS/WY/291-Dewey/Vertical.pdf",
-          "vyear": "1957",
-          "horizontal": "https://geodata.thesurveystation.com/USGS/WY/291-Dewey/Horizontal.pdf",
-          "hyear": "1957"
-        }
+      "properties": {
+        "name": "Mule Creek",
+        "vertical": "https://geodata.thesurveystation.com/USGS/WY/262-Mule-Creek/Vertical.pdf",
+        "vyear": "1970",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/WY/262-Mule-Creek/Horizontal.pdf",
+        "hyear": "1957"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-104, 43.5], [-104, 43.75], [-104.25, 43.75], [-104.25, 43.5], [-104, 43.5]]]
       },
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Polygon",
-          "coordinates": [[[-104,43.75],[-104,44],[-104.25,44],[-104.25,43.75],[-104,43.75]]]
-        },
-        "properties": {
-          "name": "Newcastle",
-          "vertical": "https://geodata.thesurveystation.com/USGS/WY/320-Newcastle/Vertical.pdf",
-          "vyear": "1957",
-          "horizontal": "https://geodata.thesurveystation.com/USGS/WY/320-Newcastle/Horizontal.pdf",
-          "hyear": "1957"
-        }
+      "properties": {
+        "name": "Dewey",
+        "vertical": "https://geodata.thesurveystation.com/USGS/WY/291-Dewey/Vertical.pdf",
+        "vyear": "1957",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/WY/291-Dewey/Horizontal.pdf",
+        "hyear": "1957"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-110.5, 43.5], [-110.5, 43.75], [-110.75, 43.75], [-110.75, 44], [-110.5, 44]]]
       },
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Polygon",
-          "coordinates": [[[-104,44],[-104,44.25],[-104.25,44.25],[-104.25,44],[-104,44]]]
-        },
-        "properties": {
-          "name": "Four Corners",
-          "vertical": "https://geodata.thesurveystation.com/USGS/WY/349-Four-Corners/Vertical.pdf",
-          "vyear": "1969",
-          "horizontal": "https://geodata.thesurveystation.com/USGS/WY/349-Four-Corners/Horizontal.pdf",
-          "hyear": "1960"
-        }
+      "properties": {
+        "name": "Grand Teton No. 4",
+        "vertical": "https://geodata.thesurveystation.com/USGS/WY/317-Grand-Teton-No-4/Vertical.pdf",
+        "vyear": "1957",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/WY/317-Grand-Teton-No-4/Horizontal.pdf",
+        "hyear": "1957"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-110.75, 43.5], [-110.75, 43.75], [-111, 43.75], [-111, 44], [-110.75, 44]]]
       },
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Polygon",
-          "coordinates": [[[-104,44.25],[-104,44.5],[-104.25,44.5],[-104.25,44.25],[-104,44.25]]]
-        },
-        "properties": {
-          "name": "Tinton",
-          "vertical": "https://geodata.thesurveystation.com/USGS/WY/378-Tinton/Vertical.pdf",
-          "vyear": "1957",
-          "horizontal": "https://geodata.thesurveystation.com/USGS/WY/378-Tinton/Horizontal.pdf",
-          "hyear": "1958"
-        }
+      "properties": {
+        "name": "Grand Teton No. 3",
+        "vertical": "https://geodata.thesurveystation.com/USGS/WY/318-Grand-Teton-No-3/Vertical.pdf",
+        "vyear": "1957",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/WY/318-Grand-Teton-No-3/Horizontal.pdf",
+        "hyear": "1957"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-104, 43.75], [-104, 44], [-104.25, 44], [-104.25, 43.75], [-104, 43.75]]]
       },
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Polygon",
-          "coordinates": [[[-104,44.5],[-104,44.75],[-104.25,44.75],[-104.25,44.5],[-104,44.5]]]
-        },
-        "properties": {
-          "name": "Beulah",
-          "vertical": "https://geodata.thesurveystation.com/USGS/WY/407-Beulah/Vertical.pdf",
-          "vyear": "1957",
-          "horizontal": "https://geodata.thesurveystation.com/USGS/WY/407-Beulah/Horizontal.pdf",
-          "hyear": "1967"
-        }
+      "properties": {
+        "name": "Newcastle",
+        "vertical": "https://geodata.thesurveystation.com/USGS/WY/320-Newcastle/Vertical.pdf",
+        "vyear": "1957",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/WY/320-Newcastle/Horizontal.pdf",
+        "hyear": "1957"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-110.5, 43.75], [-110.5, 44], [-110.75, 44], [-110.75, 43.75], [-110.5, 43.75]]]
       },
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Polygon",
-          "coordinates": [[[-104,44.75],[-104,45],[-104.25,45],[-104.25,44.75],[-104,44.75]]]
-        },
-        "properties": {
-          "name": "Colony",
-          "vertical": "https://geodata.thesurveystation.com/USGS/WY/436-Colony/Vertical.pdf",
-          "vyear": "1968",
-          "horizontal": "https://geodata.thesurveystation.com/USGS/WY/436-Colony/Horizontal.pdf",
-          "hyear": "1964"
-        }
+      "properties": {
+        "name": "Grand Teton No. 1",
+        "vertical": "https://geodata.thesurveystation.com/USGS/WY/346-Grand-Teton-No-1/Vertical.pdf",
+        "vyear": "1957",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/WY/346-Grand-Teton-No-1/Horizontal.pdf",
+        "hyear": "1957"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-104, 44], [-104, 44.25], [-104.25, 44.25], [-104.25, 44], [-104, 44]]]
       },
-    ]
-  };
+      "properties": {
+        "name": "Four Corners",
+        "vertical": "https://geodata.thesurveystation.com/USGS/WY/349-Four-Corners/Vertical.pdf",
+        "vyear": "1969",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/WY/349-Four-Corners/Horizontal.pdf",
+        "hyear": "1960"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-104, 44.25], [-104, 44.5], [-104.25, 44.5], [-104.25, 44.25], [-104, 44.25]]]
+      },
+      "properties": {
+        "name": "Tinton",
+        "vertical": "https://geodata.thesurveystation.com/USGS/WY/378-Tinton/Vertical.pdf",
+        "vyear": "1957",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/WY/378-Tinton/Horizontal.pdf",
+        "hyear": "1958"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-110.5, 44.25], [-110.5, 44.5], [-110.75, 44.5], [-110.75, 44.25], [-110.5, 44.25]]]
+      },
+      "properties": {
+        "name": "West Thumb",
+        "vertical": "https://geodata.thesurveystation.com/USGS/WY/404-West-Thumb/Vertical.pdf",
+        "vyear": "1957",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/WY/404-West-Thumb/Horizontal.pdf",
+        "hyear": "1958"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-104, 44.5], [-104, 44.75], [-104.25, 44.75], [-104.25, 44.5], [-104, 44.5]]]
+      },
+      "properties": {
+        "name": "Beulah",
+        "vertical": "https://geodata.thesurveystation.com/USGS/WY/407-Beulah/Vertical.pdf",
+        "vyear": "1957",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/WY/407-Beulah/Horizontal.pdf",
+        "hyear": "1967"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-110.25, 44.5], [-110.25, 44.75], [-110.5, 44.75], [-110.5, 44.5], [-110.25, 44.5]]]
+      },
+      "properties": {
+        "name": "Canyon Village",
+        "vertical": "https://geodata.thesurveystation.com/USGS/WY/432-Canyon-Village/Vertical.pdf",
+        "vyear": "1957",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/WY/432-Canyon-Village/Horizontal.pdf",
+        "hyear": "1967"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [[[-104, 44.75], [-104, 45], [-104.25, 45], [-104.25, 44.75], [-104, 44.75]]]
+      },
+      "properties": {
+        "name": "Colony",
+        "vertical": "https://geodata.thesurveystation.com/USGS/WY/436-Colony/Vertical.pdf",
+        "vyear": "1968",
+        "horizontal": "https://geodata.thesurveystation.com/USGS/WY/436-Colony/Horizontal.pdf",
+        "hyear": "1964"
+      }
+    },
+  ]
+};
