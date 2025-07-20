95141438c252dd87101c0477d130be04313b0772
Jennifer Galas <jennifer.galas@scranton.edu>
2025-07-20 08:39:28 -0400

    Add SD quads

--- assets/usgs-datasheets/data/south-dakota.js
+++ assets/usgs-datasheets/data/south-dakota.js
@@ -33,7 +33,7 @@ var geojsonFeature = {
         "type": "Feature",
         "geometry": {
           "type": "Polygon",
-          "coordinates": [[[-96.75,42.25],[-96.75,42.5],[-97,42.5],[-97,42.25],[-96.75,42.25]]]
+          "coordinates": [[[-96.25,42.5],[-96.25,42.75],[-96.5,42.75],[-96.5,42.5],[-96.25,42.5]]]
         },
         "properties": {
           "name": "Sioux City North",
@@ -42,6 +42,272 @@ var geojsonFeature = {
           "horizontal": "https://geodata.thesurveystation.com/USGS/SD/3-Sioux-City-North/Horizontal.pdf",
           "hyear": "1967"
         }
-      }    
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-96.5,42.5],[-96.5,42.75],[-96.75,42.75],[-96.75,42.5],[-96.5,42.5]]]
+        },
+        "properties": {
+          "name": "Elk Point",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/4-Elk-Point/Vertical.pdf",
+          "vyear": "1967",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/4-Elk-Point/Horizontal.pdf",
+          "hyear": "1967"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-96.75,42.5],[-96.75,42.75],[-97,42.75],[-97,42.5],[-96.75,42.5]]]
+        },
+        "properties": {
+          "name": "Martinsburg",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/5-Martinsburg/Vertical.pdf",
+          "vyear": "1962",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/5-Martinsburg/Horizontal.pdf",
+          "hyear": "1967"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-96.5,42.75],[-96.5,43],[-96.75,43],[-96.75,42.75],[-96.5,42.75]]]
+        },
+        "properties": {
+          "name": "Akron",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/6-Akron/Vertical.pdf",
+          "vyear": "1967",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/6-Akron/Horizontal.pdf",
+          "hyear": "1968"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-96.75,42.75],[-96.75,43],[-97,43],[-97,42.75],[-96.75,42.75]]]
+        },
+        "properties": {
+          "name": "Vermillion",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/7-Vermillion/Vertical.pdf",
+          "vyear": "1967",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/7-Vermillion/Horizontal.pdf",
+          "hyear": "1968"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-97,42.75],[-97,43],[-97.25,43],[-97.25,42.75],[-97,42.75]]]
+        },
+        "properties": {
+          "name": "Gayville",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/8-Gayville/Vertical.pdf",
+          "vyear": "1967",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/8-Gayville/Horizontal.pdf",
+          "hyear": "1968"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-97.25,42.75],[-97.25,43],[-97.5,43],[-97.5,42.75],[-97.25,42.75]]]
+        },
+        "properties": {
+          "name": "Yankton",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/9-Yankton/Vertical.pdf",
+          "vyear": "1967",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/9-Yankton/Horizontal.pdf",
+          "hyear": "1967"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-97.5,42.75],[-97.5,43],[-97.75,43],[-97.75,42.75],[-97.5,42.75]]]
+        },
+        "properties": {
+          "name": "Tabor",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/10-Tabor/Vertical.pdf",
+          "vyear": "1989",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/10-Tabor/Horizontal.pdf",
+          "hyear": "1967"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-97.75,42.75],[-97.75,43],[-98,43],[-98,42.75],[-97.75,42.75]]]
+        },
+        "properties": {
+          "name": "Tyndall No. 2",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/11-Tyndall-No-2/Vertical.pdf",
+          "vyear": "1975",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/11-Tyndall-No-2/Horizontal.pdf",
+          "hyear": "1954"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-98,42.75],[-98,43],[-98.25,43],[-98.25,42.75],[-98,42.75]]]
+        },
+        "properties": {
+          "name": "Niobrara",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/12-Niobrara/Vertical.pdf",
+          "vyear": "1975",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/12-Niobrara/Horizontal.pdf",
+          "hyear": "1971"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-98.25,42.75],[-98.25,43],[-98.5,43],[-98.5,42.75],[-98.25,42.75]]]
+        },
+        "properties": {
+          "name": "Lynch",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/13-Lynch/Vertical.pdf",
+          "vyear": "1964",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/13-Lynch/Horizontal.pdf",
+          "hyear": "1963"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-98.5,42.75],[-98.5,43],[-98.75,43],[-98.75,42.75],[-98.5,42.75]]]
+        },
+        "properties": {
+          "name": "Briston",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/14-Briston/Vertical.pdf",
+          "vyear": "1962",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/14-Briston/Horizontal.pdf",
+          "hyear": "1962"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-96.25,43],[-96.25,43.25],[-96.5,43.25],[-96.5,43],[-96.25,43]]]
+        },
+        "properties": {
+          "name": "Hudson",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/15-Hudson/Vertical.pdf",
+          "vyear": "1970",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/15-Hudson/Horizontal.pdf",
+          "hyear": "1967"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-96.5,43],[-96.5,43.25],[-96.75,43.25],[-96.75,43],[-96.5,43]]]
+        },
+        "properties": {
+          "name": "Alcester",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/16-Alcester/Vertical.pdf",
+          "vyear": "1967",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/16-Alcester/Horizontal.pdf",
+          "hyear": "1967"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-96.75,43],[-96.75,43.25],[-97,43.25],[-97,43],[-96.75,43]]]
+        },
+        "properties": {
+          "name": "Beresford",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/17-Beresford/Vertical.pdf",
+          "vyear": "1967",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/17-Beresford/Horizontal.pdf",
+          "hyear": "1967"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-97,43],[-97,43.25],[-97.25,43.25],[-97.25,43],[-97,43]]]
+        },
+        "properties": {
+          "name": "Irene",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/18-Irene/Vertical.pdf",
+          "vyear": "1968",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/18-Irene/Horizontal.pdf",
+          "hyear": "1969"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-97.25,43],[-97.25,43.25],[-97.5,43.25],[-97.5,43],[-97.25,43]]]
+        },
+        "properties": {
+          "name": "Marindahl",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/19-Marindahl/Vertical.pdf",
+          "vyear": "1967",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/19-Marindahl/Horizontal.pdf",
+          "hyear": "1970"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-97.5,43],[-97.5,43.25],[-97.75,43.25],[-97.75,43],[-97.5,43]]]
+        },
+        "properties": {
+          "name": "Scotland",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/20-Scotland/Vertical.pdf",
+          "vyear": "1960",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/20-Scotland/Horizontal.pdf",
+          "hyear": "1971"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-97.75,43],[-97.75,43.25],[-98,43.25],[-98,43],[-97.75,43]]]
+        },
+        "properties": {
+          "name": "Tripp",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/21-Tripp/Vertical.pdf",
+          "vyear": "1975",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/21-Tripp/Horizontal.pdf",
+          "hyear": "1901"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[-98,43],[-98,43.25],[-98.25,43.25],[-98.25,43],[-98,43]]]
+        },
+        "properties": {
+          "name": "Armour No. 4",
+          "vertical": "https://geodata.thesurveystation.com/USGS/SD/22-Armour-No-4/Vertical.pdf",
+          "vyear": "1975",
+          "horizontal": "https://geodata.thesurveystation.com/USGS/SD/22-Armour-No-4/Horizontal.pdf",
+          "hyear": "1901"
+        }
+      }
     ]
   };
